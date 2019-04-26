eb5bafeebcfd7a2b1f20a6253a441fbc4e5be6b5
jerryngton <jerryngtond@gmail.com>
2019-04-26 13:32:13 +0300

    Proxy example of using

---
+++ src/com/patterns/Proxy/ClientCodeExample.java
@@ -0,0 +1,29 @@
+package com.patterns.Proxy;
+
+public class ClientCodeExample {
+    public static void main(String[] args) {
+        DownloadWorker naiveDownloader = new DownloadWorker(new ThirdPartyVideoService());
+        DownloadWorker smartDownloader = new DownloadWorker(new CacheProxyVideoService());
+
+        long naive = test(naiveDownloader);
+        long smart = test(smartDownloader);
+        System.out.print("Time saved by caching proxy: " + (naive - smart) + "ms");
+    }
+
+    private static long test(DownloadWorker downloader) {
+        long startTime = System.currentTimeMillis();
+
+        // User behavior in our app:
+        downloader.renderPopularVideos();
+        downloader.renderVideoPage("catzzzzzzzzz");
+        downloader.renderPopularVideos();
+        downloader.renderVideoPage("dancesvideoo");
+        // Users might visit the same page quite often.
+        downloader.renderVideoPage("catzzzzzzzzz");
+        downloader.renderVideoPage("someothervid");
+
+        long estimatedTime = System.currentTimeMillis() - startTime;
+        System.out.print("Time elapsed: " + estimatedTime + "ms\n");
+        return estimatedTime;
+    }
+}
